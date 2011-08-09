1718ec81ecc0f78bcbb081b8bc224af20aa5687f
Wenjun Huang <wenjunh16@gmail.com>
2011-08-08 17:24:36 -0700

    modified to match manuscript

--- countMatrixGenerator.R
+++ countMatrixGenerator.R
@@ -58,20 +58,13 @@ matrixGenerator <- function(lst) {
 
 
   for (j in 1:ncol(counts)) {
-    counts["WATER",j] <- counts["WATER",j] / sum(counts[1:21,j])
-    if (sum(counts[1:20,j]) == 0) {
-      counts[1:20,j] <- 0
+    if (sum(counts[1:21,j]) == 0) {
+      counts[1:21,j] <- 0
     }
     else
-      {counts[1:20,j] <- counts[1:20,j] / sum(counts[1:20,j])
+      {counts[1:21,j] <- counts[1:21,j] / sum(counts[1:21,j])
      }
     }
-  temp <- sum(counts["WATER",])
-  for (k in 1:ncol(counts)) {
-    counts["WATER",k] <- counts["WATER",k] / temp
-    counts[1:20,k] <- counts[1:20,k] * (1-counts["WATER",k])
-  }
-  
   counts <- counts[-c(22,23,24),]
   
   return(counts)
