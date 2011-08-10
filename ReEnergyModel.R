7a7076f77d15f2a85c1e2786c1d87c552231d394
Andrew White <whitead@uw.edu>
2011-08-09 21:56:49 -0700

    Think I fixed the code, but since I still can't access SQLShare, I'm not sure

--- ReEnergyModel.R
+++ ReEnergyModel.R
@@ -1,6 +1,6 @@
 source("SQLShareLib.R")
 
-groDist <- fetchFrame("greoldist.csv", username="wenjunh")
+groDist <- fetchFrame("groeldist.csv", username="wenjunh")
 
 energyCycle <- function(protein, distribution) {
   countMatrix <- sampleCounts(distribution)
@@ -23,31 +23,25 @@ energyCycle <- function(protein, distribution) {
   names(ener) <- c("Gclose","Gopen")
 
   ener["Gclose"] <- freeEnergyModel(countMatrix, groDist["Close", ], pfracs)
+  ener["Gopen"] <- freeEnergyModel(countMatrix, groDist["Open", ], pfracs)
 
   return(ener)
 }
 
-freeEnergyModel <- function(countMatrix,yDist,pfracs) {
+freeEnergyModel <- function(countMatrix,yDist,pfracs,bootstrap=10000) {
 
   Num <- distriNum
-
-  rownames(countMatrix) <- c("A","R","N","D","C","Q","E","G","H","I","L","K","M","F","P","S","T","W","Y","V","WATER")
-  colnames(countMatrix) <- c("A","R","N","D","C","Q","E","G","H","I","L","K","M","F","P","S","T","W","Y","V")
-
-  countMatrix <- countMatrix[order(rownames(countMatrix)),order(colnames(countMatrix))]
+  
   water <- array(0,ncol(countMatrix))
   for (l in 1:ncol(countMatrix)) {
     water[l] <- countMatrix["WATER",l] / sum(countMatrix["WATER",])
   }
   names(water) <- colnames(countMatrix)
-#  print(water)
-#  print(countMatrix)
+  print(water)
+  print(countMatrix)
   
   deltaGOpen <- array(0,1000)
   deltaGClose <- array(0,1000)
-
-
-  sfracGrEL <- list(sfracGrELClose_1C, sfracGrELOpen_1C)
 
   sigmaXln <- array(0,2)
   sigmaX <- array(0,2)
@@ -57,27 +51,27 @@ freeEnergyModel <- function(countMatrix,yDist,pfracs) {
     for (j in 1:ncol(pfracs[[i]])) {
        Y <- array(0, length(yDist))
        for (k in 1:length(yDist)) {
-         Y[k] <- countMatrix[which(rownames(countMatrix) == colnames(pfracs[[i]][j])), which(colnames(countMatrix) == names(yDist[k]))] * yDist[k] * (1-countMatrix[which(rownames(countMatrix) == "WATER"), which(colnames(countMatrix) == names(yDist[k]))]) + water[which(names(water) == colnames(pfracs[[i]][j]))] * countMatrix[which(rownames(countMatrix) == "WATER"), which(colnames(countMatrix) == names(yDist[k]))] * yDist[k] 
+         Y[k] <- countMatrix[colnames(pfracs[[i]][j]), names(yDist[k])] * yDist[k] * (1 - countMatrix["WATER", names(yDist[k])]) +
+           water[colnames(pfracs[[i]][j]))] * countMatrix["WATER", names(yDist[k]))] * yDist[k] 
        }
-#       print(Y)
-       sigmaY <- log(sum(Y), base = exp(1))
-#       print(sigmaY)
+       print(Y)
+       sigmaY <- log(sum(Y))
+       print(sigmaY)
        X[j] <- mean(pfracs[[i]][,j]) * sigmaY
      }
     sigmaXln[i] <- sum(X)
-    sigmaX[i] <- exp(sum(X))
+    sigmaX[i] <- sum(exp(X))
   }
   deltaG <- array(0,2)
   names(deltaG) <- c("deltaGln","deltaG")
   deltaG[1] <- sigmaXln[1]-sigmaXln[2]
-  deltaG[2] <- sigmaX[1]-sigmaX[2]
+  deltaG[2] <- sigmaX[1]/sigmaX[2]
   return (deltaG)
 
 }
 
 energybootstrap <- function(bootstrap,protein,distribution) {
-  bootstrap <- bootstrap
-  source('obtainSurfaceResidues.R')
+
   #  obtain surface residues for both E.Coli fold and E.Coli unfold
   cutoff <- 0.3
   pfracsfold <- fetchAllSurfResidues(protein, cutoff, normalize=TRUE)
