4a64aa5177421498e90440881545adf75d15bfb2
Wenjun Huang <wenjunh16@gmail.com>
2011-12-21 14:40:33 -0800

    cleaned up used code

--- getsurfresidue.py
+++ getsurfresidue.py
@@ -20,20 +20,15 @@ data = []
 center = [0,0,0]
 atomnum = [0]
 
-#with open(outf, 'w') as g:
-#    with open(outf2, 'w') as k:
-#        g.write("Chain Rnum Res SA\n")
-#        k.write("Chain Rnum Atom x y z\n")
-
 #Calculate the center of the molecule
 for n in range(0,len(f)):
     r1 = f.getResidues(n)
     if r1.isSurf() == True :
-               #g.write("%s %s %s %s\n" %(r1.getChain(), r1.getIndex(), r1.getType(), r1.getSA()))
+
         atomnum[0] += r1.getAtomNum()
         for j in range(0,r1.getAtomNum()):
             a1 = r1.getSingleAtom(j)
-                   #k.write("%s %s %s %s %s %s\n" %(r1.getChain(), r1.getIndex(), a1.getType(), a1.getCoord()[0], a1.getCoord()[1], a1.getCoord()[2]))
+
             center[0] += a1.getCoord()[0]
             center[1] += a1.getCoord()[1]
             center[2] += a1.getCoord()[2]
@@ -43,14 +38,12 @@ center[1] = center[1] / atomnum[0]
 center[2] = center[2] / atomnum[0]
 print("Center Point Coordinate is %s" %(center))
 
-#with open(outf3, 'w') as d:
-#    d.write("Chain Rnum Atom dist\n")
 
 #Not sure what this part does
 for n in range(0,len(f)):
     for j in range(0,f.getResidues(n).getAtomNum()):
         a1 = f.getResidues(n).getSingleAtom(j)
-            #d.write("%s %s %s %s\n" %(f.getResidues(n).getChain(), f.getResidues(n).getIndex(), a1.getType(), a1.centerdistance(center)))
+
 
 with open(outf, 'w') as g:
     g.write("PDBID, Chain, Rnum, Res, Location, SAfrac\n")
