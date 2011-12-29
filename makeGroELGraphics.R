e9c9ae7e51f37fea07e9262db6b49680ca6071df
Wenjun Huang <wenjunh16@gmail.com>
2011-12-28 16:00:51 -0800

    merged with makeGroELPaperGraphics.R

--- makeGroELGraphics.R
+++ makeGroELGraphics.R
@@ -36,19 +36,69 @@ error.bar(barx, yy, ee, length=0.01)
 legend("topright", col=c("gray90", "gray30"), legend=c("E. Coli Surface", "E. Coli All"), ncol=1, pch=15)
 graphics.off()
 
-#plot open/close with error bars
 
+#plot1(a) ecoli distribution with error bars
 
+#efracs <- fetchAllSurfResidues("ecoli40", cutoff, normalize=TRUE, username="wenjunh")
+#
+#ems <- apply(efracs, MARGIN=2, mean)
+#eci <- apply(efracs, MARGIN=2, FUN=function(x) matrix(c(quantile(x, c(0.975)) - median(x), median(x) - quantile(x, c(0.025))), byrow=T, nrow=2))
+#ese <- apply(efracs, MARGIN=2, FUN=function(x) matrix(c(median(x) + sd(x) / sqrt(nrow(efracs)) , median(x) - sd(x) / sqrt(nrow(efracs))), byrow=T, nrow=2))
+#
 #sql <- paste("select * FROM [wenjunh@washington.edu].[groel_insurfres_count.csv]")
 #rawData <- fetchdata(sql)
-#groDist <- empty.df(rawData[[1]][[1]][-1], c(rawData[[1]][[2]][1], rawData[[1]][[3]][1]))
-#groDist[1:2,] <- matrix(unlist(lapply(rawData[[1]][-1], function(row) sapply(row[-1], as.integer))), nrow=2, byrow=T)
+#sql2 <- paste("select * FROM [wenjunh@washington.edu].[groel_outsurfres_count.csv]")
+#rawData2 <- fetchdata(sql2)
 #
-#groDist[1, ] <- as.double(groDist["GroEL_Close", ]) / sum(as.double(groDist["GroEL_Close", ]))
-#groDist[2, ] <- as.double(groDist["GroEL_Open", ]) / sum(as.double(groDist["GroEL_Open", ]))
+#groDist <- empty.df(rawData[[1]][[1]][-1], c('ecoli', rawData[[1]][[2]][1], rawData[[1]][[3]][1], rawData2[[1]][[2]][1]))
+#groDist[1,] <- ems
+#groDist[2:3,] <- matrix(unlist(lapply(rawData[[1]][-1], function(row) sapply(row[-1], as.integer))), nrow=2, byrow=T)
+#groDist[4,] <- matrix(unlist(lapply(rawData2[[1]][-1], function(row) sapply(row[-1], as.integer))), nrow=1) 
 #
-#tiff("groELDist.tiff", width=3.3*500, height=2.5*500, pointsize=8, res=500)
-#par(mar=c(3,4,0.5,0.5), cex.axis=0.65, family="LMSans10")
-#barplot(as.matrix(groDist[c(2,1),]), col=c("gray90", "gray25"), main="", xlab="Amino Acid", ylab="Density", beside=T, names.arg=aalist.sh)
-#legend("topright", col=c("gray90", "gray25"), legend=c("GroEL Open", "GroEL Close"), ncol=1, pch=15)
+#groDist[2, ] <- as.double(groDist["GroEL_Close", ]) / sum(as.double(groDist["GroEL_Close", ]))
+#groDist[3, ] <- as.double(groDist["GroEL_Open", ]) / sum(as.double(groDist["GroEL_Open", ]))
+#groDist[4, ] <- as.double(groDist["GroEL_Out", ]) / sum(as.double(groDist["GroEL_Out", ]))
+#
+#b <- array(0,20)
+#ese.up <- matrix(c(ese[1,],b,b,b), byrow=T, nrow=4)
+#ese.low <- matrix(c(ese[2,],b,b,b), byrow=T, nrow=4)
+#
+#cairo_pdf('groDist_SE.pdf', width=3.42, height=2.58)
+##tiff("groDist_SE.tiff", width=3.42, height=2.58) 
+#par(family="LMSans10", cex.axis=0.65)
+#barx <- barplot(as.matrix(groDist), col=c("gray90","gray75","gray60","gray45"), main="", xlab="Amino Acid", ylab="Density", beside=T, names.arg=aalist.sh, ylim = c(0.00,0.20))
+##error.bar(barx, groDist, upper=ese.up, lower=ese.low)#, length=0.01)
+##legend("topright", col=c("gray90","gray75","gray60","gray45", legend=c("E.Coli Surface","GroEL Close","GroEL Open","GroEL Out")))
 #graphics.off()
+
+
+#plot2 The plot of 5 species
+
+hspRawCount <- fetchHSPResCount("hsp","In","wenjunh")
+
+for (i in 1:nrow(hspRawCount)) {
+  hspRawCount[i,] <- hspRawCount[i,] / sum(hspRawCount[i,])
+}
+hspRawCount <- as.matrix(hspRawCount)
+
+hspDist <- matrix(0,nrow(hspRawCount)+1, ncol(hspRawCount))
+rownames(hspDist) <- c("E.Coli GroEl","Thermo GroEl","Group II HSP","HSP90","Eukaryotic HSP","E.Coli")
+colnames(hspDist) <- colnames(hspRawCount)
+
+hspDist[1,] <- hspRawCount["1SX4",]
+hspDist[2,] <- hspRawCount["1WE3",]
+hspDist[3,] <- hspRawCount["3KFB",]
+hspDist[4,] <- hspRawCount["2CG9",]
+hspDist[5,] <- hspRawCount["3P9D",]
+
+cutoff <- 0.3
+psurf <- fetchAllSurfResidues("ecoli", cutoff, normalize=TRUE, "wenjunh")
+hspDist[6,] <- apply(psurf, MARGIN=2, FUN=mean)
+
+cairo_pdf('HSP_Protein_Dist.pdf',width=5.5, height=2.58, pointsize=9)
+par(family='LMSans10', cex.axis=0.75)
+barplot(hspDist, beside=TRUE, col=c('gray90','gray75','gray60','gray45','gray30','gray15'), ylim=c(0.0,0.3), names.arg=aalist.sh,)
+legend("topright", col=c('gray90','gray75','gray60','gray45','gray30','gray15'), legend=c("E.Coli GroEl","Thermo GroEl","Group II HSP","HSP90","Eukaryotic HSP","E.Coli"), pch=rep(15,6), cex=0.8)
+graphics.off()
+
+
