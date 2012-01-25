8e9adf7091bdcb1c581851de57eabf5ffdf24096
Andrew White <white.d.andrew@gmail.com>
2012-01-24 16:40:50 -0800

    Added a few new graphs to renergy model and changed the getInteriorResidue file to take an extra parameter

---
+++ getInteriorResidues.py
@@ -0,0 +1,169 @@
+#! /usr/bin/python
+
+from psurf import *
+import sys, math
+
+
+def readChains(chainFile):
+    chains = []
+    with open(chainFile, 'r') as f:
+        for line in f.readlines():
+            chains.append(line.split())
+    return chains
+
+def printHelp():
+    print "getInteriorResidues.py [input pdb] [chain file] [surface cutoff] [margin] [center axis unit vector]"
+    print "The chain file should have a list of chains which are identical on each line. Only the first chain of each line will be calculated and the results will be replicated"
+
+def testIntersectAlgorithm(origin, points, centralAxis):
+    for p in points:
+        #Get the point on the central axis that is closest to the origin atom
+        #cv = s * axis, s = P_o dot axis. Assuming axis is a unit vector
+        tempDP = sum([x * y for x,y in zip(origin, centralAxis)])
+        centerVector = [x * tempDP for x in centralAxis]
+        
+        avector = [x - y for x,y in zip(p, origin)]
+        bvector = [x - y for x,y in zip(centerVector, origin)]
+        
+            #Check to see if we need to consider occlusion (because the other atom is too far away)
+        if(sum([x**2 for x in avector]) > sum([x**2 for x in bvector])):
+            print "Bad distance"
+            continue
+        
+            #|A|^2 - (A * B)^2 / |B|^2
+        
+            #(A * B)
+        tempDP = sum([x * y for x,y in zip(avector, bvector)])
+        
+            #Check if other atom is "behind" the first atom. As in the angle is obtuse
+        if(tempDP < 0):
+            print "Bad angle"
+            continue
+        
+        radiusSqr = sum([x**2 for x in avector]) - tempDP**2 / sum([x**2 for x in bvector])
+        
+        print(radiusSqr)
+
+def getInterior(chain, protein, centralAxis, minMax, margin, rmax=65, heavyMargin = 3):
+    residueIndices = []
+    count = 0
+    rlength = len(protein.getResidues(chain))
+    for rorigin in protein.getResidues(chain):
+        if(rorigin.isSurf()):
+            for atom in rorigin.getAtoms():
+                if(atom.isHeavy()):
+                    atom.occluded = False
+            for rother in protein.getResidues():
+                if(rother != rorigin and rother.isSurf()):
+                    centerIntersect(rorigin, rother, centralAxis, minMax, margin, rmax)
+
+            clearCount = 0
+            heavyCount = 0
+            for atom in rorigin.getAtoms():
+                if(atom.isHeavy()):
+                    heavyCount += 1
+                    if(not atom.occluded):
+                        clearCount += 1
+            if(clearCount >= heavyMargin):#float(clearCount) / heavyCount >= heavyMargin):
+                residueIndices.append(rorigin.getIndex())
+
+        count += 1
+    return residueIndices
+                    
+            
+def centerIntersect(rorigin, rother, centralAxis, minMax, margin, rmax):
+    for a1 in rorigin.getAtoms():
+        if(not a1.isHeavy()):
+            continue
+        for a2 in rother.getAtoms():
+            if(not a2.isHeavy()):
+                continue
+
+            #Get the point on the central axis that is closest to the origin atom
+            #cv = s * axis, s = P_o dot axis. Assuming axis is a unit vector
+            tempDP = sum([x * y for x,y in zip(a1.getCoord(), centralAxis)])
+            if(tempDP < minMax[0] or tempDP > minMax[1]):
+                a1.occluded = True
+                continue
+
+            centerVector = [x * tempDP for x in centralAxis]
+
+            avector = [x - y for x,y in zip(a2.getCoord(), a1.getCoord())]
+            bvector = [x - y for x,y in zip(centerVector, a1.getCoord())]
+
+            #Check if the atom is too far away to be considered in the interior
+            if(sum([x**2 for x in bvector]) > rmax*rmax):
+                a1.occluded = True
+                continue
+            
+            #Check to see if we need to consider occlusion (because the other atom is too far away)
+            if(sum([x**2 for x in avector]) > sum([x**2 for x in bvector])):
+                continue
+
+            #|A|^2 - (A * B)^2 / |B|^2
+            
+            #(A * B)
+            tempDP = sum([x * y for x,y in zip(avector, bvector)])
+            
+            #Check if other atom is "behind" the first atom. As in the angle is obtuse
+            if(tempDP < 0):
+                continue
+            
+            radiusSqr = sum([x**2 for x in avector]) - tempDP**2 / sum([x**2 for x in bvector])
+
+            if(radiusSqr < margin**2):
+                a1.occluded = True
+
+
+def main():
+    
+    if(len(sys.argv) != 10):
+        printHelp()
+        exit()
+    
+    inputFile = sys.argv[1]
+    chainFile = sys.argv[2]
+    surfCutoff = float(sys.argv[3])
+    margin = float(sys.argv[4])
+    centralAxis = [float(x) for x in sys.argv[5:8]]
+    minMax = [float(x) for x in sys.argv[8:10]]
+ 
+    chains = readChains(chainFile)
+
+    p = readProteinSA(inputFile)
+
+    #center the protein
+    p.centerProtein()
+    
+    setSurfCutoff(surfCutoff)
+
+    for r in p.getResidues():
+        for a in r.getAtoms():
+            if(r.isSurf()):
+                a.setBeta(1.0)
+            else:
+                a.setBeta(0.0)
+
+    for chainList in chains:
+        residueIndices = getInterior(chainList[0], p, centralAxis, minMax, margin)
+        for c in chainList:
+            print c
+            for ri in residueIndices:
+                print ri
+                res = p.getResidue(ri, c)
+                if(res):
+                    for a in res.getAtoms():
+                        a.setBeta(-1.0)
+                else:
+                    print "Could not find residue index %d in chain %s" % (ri, c)
+    p.writePDB("%s_beta.pdb" % p.getName())
+
+    #write an XYZ file containing the interior CA's
+    p.writeXYZAtoms("%s_beta.xyz" % p.getName(), beta=-1)
+
+main()
+
+#points = [[2,3,0], [3,2,0], [1.95,1.75, 0], [-1.95,1.75, 0], [-2,1, 0], [-1.95,-1.75, 0], [2.1, 1, 0]]
+#origin = [2,1,0]
+#ca = [0,1,0]
+#testIntersectAlgorithm(origin, points, ca)
--- makeGroELGraphics.R
+++ makeGroELGraphics.R
@@ -2,79 +2,65 @@
 source("SQLShareLib.R")
 #plot ecoli distribution with error bars
 
-efracs <- fetchAllSurfResidues("ecoli", cutoff, normalize=TRUE, username="wenjunh")
-ufracs <- fetchAllSurfResidues("ecoli", -1., normalize=TRUE, username="wenjunh")
-ems <- apply(efracs, MARGIN=2, mean)
-eci <- apply(efracs, MARGIN=2, FUN=function(x) matrix(c(quantile(x, c(0.975)) - median(x), median(x) - quantile(x, c(0.025))), byrow=T, nrow=2))
-ums <- apply(ufracs, MARGIN=2, mean)
-uci <- apply(ufracs, MARGIN=2, FUN=function(x) matrix(c(quantile(x, c(0.975)) - median(x), median(x) - quantile(x, c(0.025))), byrow=T, nrow=2))
+#efracs <- fetchAllSurfResidues("ecoli", cutoff, normalize=TRUE, username="wenjunh")
+#ufracs <- fetchAllSurfResidues("ecoli", -1., normalize=TRUE, username="wenjunh")
+#ems <- apply(efracs, MARGIN=2, mean)
+ #eci <- apply(efracs, MARGIN=2, FUN=function(x) matrix(c(quantile(x, c(0.975)) - median(x), median(x) - quantile(x, c(0.025))), byrow=T, nrow=2))
+#ums <- apply(ufracs, MARGIN=2, mean)
+#uci <- apply(ufracs, MARGIN=2, FUN=function(x) matrix(c(quantile(x, c(0.975)) - median(x), median(x) - quantile(x, c(0.025))), byrow=T, nrow=2))
 
 
-tiff("ecolidist_ci.tiff", width=3.3*500, height=2.5*500, pointsize=8, res=500)
-par(mar=c(3,4,0.5,0.5), cex.axis=0.65, family="LMSans10")
-yy <- matrix(c(ems,ums), byrow=T, nrow=2)
-print(yy)
-ee.up <- matrix(c(eci[1,], uci[1,]), byrow=T, nrow=2)
-ee.low <- matrix(c(eci[2,], uci[2,]), byrow=T, nrow=2)
-barx <- barplot(yy, col=c("gray90", "gray30"), main="", xlab="Amino Acid", ylab="Density", beside=T, names.arg=aalist.sh,ylim = c(0.00,0.14))#ylim=c(0,ee.up[which.max(yy)] + max(yy)))
-error.bar(barx, yy, upper=ee.up, lower=ee.low,length=0.01)
-legend("topright", col=c("gray90", "gray30"), legend=c("E. Coli Surface", "E. Coli All"), ncol=1, pch=15)
-graphics.off()
+#tiff("ecolidist_ci.tiff", width=3.3*500, height=2.5*500, pointsize=8, res=500)
+#par(mar=c(3,4,0.5,0.5), cex.axis=0.65, family="LMSans10")
+#yy <- matrix(c(ems,ums), byrow=T, nrow=2)
+#print(yy)
+#ee.up <- matrix(c(eci[1,], uci[1,]), byrow=T, nrow=2)
+#ee.low <- matrix(c(eci[2,], uci[2,]), byrow=T, nrow=2)
+#barx <- barplot(yy, col=c("gray90", "gray30"), main="", xlab="Amino Acid", ylab="Density", beside=T, names.arg=aalist.sh,ylim = c(0.00,0.14))#ylim=c(0,ee.up[which.max(yy)] + max(yy)))
+#error.bar(barx, yy, upper=ee.up, lower=ee.low,length=0.01)
+#legend("topright", col=c("gray90", "gray30"), legend=c("E. Coli Surface", "E. Coli All"), ncol=1, pch=15)
+#graphics.off()
 
-evs <- apply(efracs, MARGIN=2, var)
-uvs <- apply(ufracs, MARGIN=2, var)
+#evs <- apply(efracs, MARGIN=2, var)
+#uvs <- apply(ufracs, MARGIN=2, var)
 
 
 
 tiff("ecolidist.tiff", width=3.3*500, height=2.5*500, pointsize=8, res=500)
-par(mar=c(3,4,0.5,0.5), cex.axis=0.65, family="LMSans10")
-yy <- matrix(c(ems,ums), byrow=T, nrow=2)
-ee <- matrix(c(sqrt(evs / nrow(efracs)), sqrt(uvs / nrow(ufracs))), byrow=T, nrow=2)
-print(ee)
-barx <- barplot(yy, col=c("gray90", "gray30"), main="", xlab="Amino Acid", ylab="Density", beside=T, names.arg=aalist.sh,ylim = c(0.00,0.14))#ylim=c(0,ee[which.max(yy)] + max(yy)))
-error.bar(barx, yy, ee, length=0.01)
-legend("topright", col=c("gray90", "gray30"), legend=c("E. Coli Surface", "E. Coli All"), ncol=1, pch=15)
-graphics.off()
-
-
-#plot1(a) ecoli distribution with error bars
-
-#efracs <- fetchAllSurfResidues("ecoli40", cutoff, normalize=TRUE, username="wenjunh")
-#
-#ems <- apply(efracs, MARGIN=2, mean)
-#eci <- apply(efracs, MARGIN=2, FUN=function(x) matrix(c(quantile(x, c(0.975)) - median(x), median(x) - quantile(x, c(0.025))), byrow=T, nrow=2))
-#ese <- apply(efracs, MARGIN=2, FUN=function(x) matrix(c(median(x) + sd(x) / sqrt(nrow(efracs)) , median(x) - sd(x) / sqrt(nrow(efracs))), byrow=T, nrow=2))
-#
-#sql <- paste("select * FROM [wenjunh@washington.edu].[groel_insurfres_count.csv]")
-#rawData <- fetchdata(sql)
-#sql2 <- paste("select * FROM [wenjunh@washington.edu].[groel_outsurfres_count.csv]")
-#rawData2 <- fetchdata(sql2)
-#
-#groDist <- empty.df(rawData[[1]][[1]][-1], c('ecoli', rawData[[1]][[2]][1], rawData[[1]][[3]][1], rawData2[[1]][[2]][1]))
-#groDist[1,] <- ems
-#groDist[2:3,] <- matrix(unlist(lapply(rawData[[1]][-1], function(row) sapply(row[-1], as.integer))), nrow=2, byrow=T)
-#groDist[4,] <- matrix(unlist(lapply(rawData2[[1]][-1], function(row) sapply(row[-1], as.integer))), nrow=1) 
-#
-#groDist[2, ] <- as.double(groDist["GroEL_Close", ]) / sum(as.double(groDist["GroEL_Close", ]))
-#groDist[3, ] <- as.double(groDist["GroEL_Open", ]) / sum(as.double(groDist["GroEL_Open", ]))
-#groDist[4, ] <- as.double(groDist["GroEL_Out", ]) / sum(as.double(groDist["GroEL_Out", ]))
-#
-#b <- array(0,20)
-#ese.up <- matrix(c(ese[1,],b,b,b), byrow=T, nrow=4)
-#ese.low <- matrix(c(ese[2,],b,b,b), byrow=T, nrow=4)
-#
-#cairo_pdf('groDist_SE.pdf', width=3.42, height=2.58)
-##tiff("groDist_SE.tiff", width=3.42, height=2.58) 
-#par(family="LMSans10", cex.axis=0.65)
-#barx <- barplot(as.matrix(groDist), col=c("gray90","gray75","gray60","gray45"), main="", xlab="Amino Acid", ylab="Density", beside=T, names.arg=aalist.sh, ylim = c(0.00,0.20))
-##error.bar(barx, groDist, upper=ese.up, lower=ese.low)#, length=0.01)
-##legend("topright", col=c("gray90","gray75","gray60","gray45", legend=c("E.Coli Surface","GroEL Close","GroEL Open","GroEL Out")))
+#par(mar=c(3,4,0.5,0.5), cex.axis=0.65, family="LMSans10")
+#yy <- matrix(c(ems,ums), byrow=T, nrow=2)
+#ee <- matrix(c(sqrt(evs / nrow(efracs)), sqrt(uvs / nrow(ufracs))), byrow=T, nrow=2)
+#print(ee)
+#barx <- barplot(yy, col=c("gray90", "gray30"), main="", xlab="Amino Acid", ylab="Density", beside=T, names.arg=aalist.sh,ylim = c(0.00,0.14))#ylim=c(0,ee[which.max(yy)] + max(yy)))
+#error.bar(barx, yy, ee, length=0.01)
+#legend("topright", col=c("gray90", "gray30"), legend=c("E. Coli Surface", "E. Coli All"), ncol=1, pch=15)
 #graphics.off()
+
+
+
+
+sql <- paste("select * FROM [whitead@washington.edu].[GroEL_counts.csv]")
+rawData <- fetchdata(sql)
+
+
+groDist <- empty.df(rawData[[1]][[1]][-1], c(rawData[[1]][[2]][1], rawData[[1]][[3]][1]))
+groDist[1:2,] <- matrix(unlist(lapply(rawData[[1]][-1], function(row) sapply(row[-1], as.integer))), nrow=2, byrow=T)
+
+groDist[1, ] <- as.double(groDist["GroEL_Close", ]) / sum(as.double(groDist["GroEL_Close", ]))
+groDist[2, ] <- as.double(groDist["GroEL_Open", ]) / sum(as.double(groDist["GroEL_Open", ]))
+
+b <- array(0,20)
+
+cairo_pdf('groDist_SE.pdf', width=3.42, height=2.58, pointsize=8)
+par(family="LMSans10", cex.axis=0.6)
+barx <- barplot(as.matrix(groDist), col=c("gray15","gray75"), main="", xlab="Amino Acid", ylab="", beside=T, names.arg=aalist.sh, ylim = c(0.00,0.20))
+legend("topright", col=c("gray15","gray75"), legend=c("GroEL-GroES (Closed)","GroEL (Open)"), pch=15)
+graphics.off()
 
 
 #plot2 The plot of 5 species
 
-hspRawCount <- fetchHSPResCount("hsp","In","wenjunh")
+hspRawCount <- fetchHSPResCount("hsp","in",username="whitead")
 
 for (i in 1:nrow(hspRawCount)) {
   hspRawCount[i,] <- hspRawCount[i,] / sum(hspRawCount[i,])
@@ -82,7 +68,7 @@ for (i in 1:nrow(hspRawCount)) {
 hspRawCount <- as.matrix(hspRawCount)
 
 hspDist <- matrix(0,nrow(hspRawCount)+1, ncol(hspRawCount))
-rownames(hspDist) <- c("E.Coli","E.Coli GroEl","Thermo GroEl","Group II HSP","HSP90","Eukaryotic HSP")
+rownames(hspDist) <- c("E.Coli","E.Coli GroEl","Thermo GroEl","Group II HSP","HSP90","Eukaryotic CCT")
 colnames(hspDist) <- colnames(hspRawCount)
 
 hspDist[2,] <- hspRawCount["1SX4",]
@@ -91,14 +77,18 @@ hspDist[4,] <- hspRawCount["3KFB",]
 hspDist[5,] <- hspRawCount["2CG9",]
 hspDist[6,] <- hspRawCount["3P9D",]
 
+#various statistics
+print(sum(hspRawCount["3KFB", c("ASP", "GLU", "ARG", "HIS")]))
+
 cutoff <- 0.3
 psurf <- fetchAllSurfResidues("ecoli", cutoff, normalize=TRUE, "wenjunh")
 hspDist[1,] <- apply(psurf, MARGIN=2, FUN=mean)
 
 cairo_pdf('HSP_Protein_Dist.pdf',width=5.5, height=2.58, pointsize=9)
 par(family='LMSans10', cex.axis=0.75)
-barplot(hspDist, beside=TRUE, col=c('gray90','gray75','gray60','gray45','gray30','gray15'), ylim=c(0.0,0.3), names.arg=aalist.sh,)
-legend("topright", col=c('gray90','gray75','gray60','gray45','gray30','gray15'), legend=c("E.Coli","E.Coli GroEl","Thermo GroEl","Group II HSP","HSP90","Eukaryotic HSP"), pch=rep(15,6), cex=0.8)
+barplot(hspDist, beside=TRUE, col=c('blue','gray75','gray60','gray45','gray30','gray15'), ylim=c(0.0,0.3), names.arg=aalist.sh,)
+legend("topright", col=c('blue','gray75','gray60','gray45','gray30','gray15'), legend=c("E.Coli","E.Coli GroEl","Thermo GroEl","Group II HSP","HSP90","Eukaryotic CCT"), pch=rep(15,6), cex=0.8)
 graphics.off()
+
 
 
